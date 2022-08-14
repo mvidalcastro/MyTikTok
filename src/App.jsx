6875ecbcb3ad715d4737e097b750368373c5ee6c
Miguel Angel Vidal <mvidalcastro@gmail.com>
2022-08-14 00:32:21 -0400

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <main>
+          TikTok Clone
+      </main>
+      
+    </div>
+  )
+}
+
+export default App
